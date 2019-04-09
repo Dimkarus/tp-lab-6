ba79ed0ee041a95895f8623b67db505de660d683
Vailina <43599573+Vailina@users.noreply.github.com>
2019-04-09 19:03:58 +0300

    Update personal.h

--- include/personal.h
+++ include/personal.h
@@ -7,22 +7,22 @@ class Personal :public Employee, public WorkTime{
 protected:
 	int rate;
 public:
-	Personal::Personal(int id, string name, int worktime, int rate):Employee(id, name, worktime){};
+	Personal(int id, string name, int worktime, int rate):Employee(id, name, worktime){};
 	int WorkTime::countByTime(){};
 	int getPayment() override{};
 }; //работник по найму с оплатой за фактически отработанное время.Имеет ставку за час.
 //third level
 class Cleaner:public Personal{
 public:
-	Cleaner::Cleaner(int id, string name, int worktime, int rate) :Personal(id, name, worktime, rate){};
-	int Cleaner::getPayment(){};
+	Cleaner(int id, string name, int worktime, int rate) :Personal(id, name, worktime, rate){};
+	int getPayment(){};
 	
 }; //уборщица.
 class Driver:public Personal{
 private:
 	int overtime;
 public:
-	Driver::Driver(int id, string name, int worktime, int rate, int overtime) :Personal(id, name, worktime, rate){};
-	int Driver::getPayment(){};
+	Driver(int id, string name, int worktime, int rate, int overtime) :Personal(id, name, worktime, rate){};
+	int getPayment(){};
 }; // водитель.
 #endif
