9fe3e22201a6b8b37c9a8c143aa8abcae61ca7c7
Vailina <43599573+Vailina@users.noreply.github.com>
2019-04-09 19:10:35 +0300

    Update employee.cpp

--- src/employee.cpp
+++ src/employee.cpp
@@ -3,7 +3,7 @@
 #include "employee.h"
 using namespace std;
 
-	Employee(int id, string name, int worktime){
+	Employee::Employee(int id, string name, int worktime){
 	this->id = id;
 	this->name = name;
 	this->worktime = worktime;
@@ -11,10 +11,6 @@ using namespace std;
 	string Employee::getName(){
 		return name;
 	}
-	virtual int getPayment() = 0;
-
-	virtual int countByTime() = 0;
-
 	Project::Project(int fund){
 		this->fund = fund;
 	}
